c50c944ec77d63151ec4383f1574bfc9dcffcdcc
Dag Odenhall <dag.odenhall@gmail.com>
2012-03-30 14:28:01 +0200

    pjax

--- src/Happaste/Html.hs
+++ src/Happaste/Html.hs
@@ -6,11 +6,10 @@ import qualified HSX.XMLGenerator as HSX
 
 import Control.Monad             (liftM)
 import Data.Lens                 ((^.))
-import HSX.JMacro                (IntegerSupply)
-import Happstack.Server          (Response, ToMessage, toResponse)
+import Happstack.Server          (Response, ToMessage, getHeaderM, toResponse)
 import Happstack.Server.HSP.HTML (EmbedAsChild(asChild), EmbedAsAttr, genElement, asAttr, Attr((:=)), XMLGenT, unXMLGenT, XMLGenerator, genEElement)
-import Language.Css.Syntax       (StyleSheet)
 import Web.Routes.XMLGenT        ()
+import Language.Javascript.JMacro
 
 import Happaste.Css (css)
 import Happaste.State
@@ -19,41 +18,51 @@ import Happaste.Types
 each :: [a] -> (a -> b) -> [b]
 each = flip map
 
-appTemplate ::
-    ( EmbedAsChild m StyleSheet
-    , EmbedAsChild m c
-    , XMLGenerator m
-    , ToMessage (HSX.XML m)
-    , EmbedAsAttr m (Attr String Sitemap)
-    , IntegerSupply m
-    ) => c -> m Response
-appTemplate body = liftM toResponse $ unXMLGenT
-    <html>
-      <head>
-        <% stylesheet $ Asset "yui.css" %>
-        <% stylesheet $ Asset "highlighter.css" %>
-        <% stylesheet "http://fonts.googleapis.com/css?family=Stoke" %>
-        <% css %>
-      </head>
-      <body>
-        <div id="header">
-          <div class="grid">
-            <div class="yui3-g">
-              <% unit "1"
-                <h1><a href=NewPaste>Happaste</a></h1>
-              %>
-            </div>
-          </div>
-        </div>
-        <div id="content">
+appTemplate :: (EmbedAsChild Server c) => c -> Server Response
+appTemplate body = do
+    pjax <- getHeaderM "X-PJAX"
+    liftM toResponse $ unXMLGenT $
+      case pjax of
+        Just _ ->
           <div class="grid">
             <div class="yui3-g">
               <% body %>
             </div>
           </div>
-        </div>
-      </body>
-    </html>
+        Nothing ->
+          <html>
+            <head>
+              <% stylesheet $ Asset "yui.css" %>
+              <% stylesheet $ Asset "highlighter.css" %>
+              <% stylesheet "http://fonts.googleapis.com/css?family=Stoke" %>
+              <% css %>
+            </head>
+            <body>
+              <div id="header">
+                <div class="grid">
+                  <div class="yui3-g">
+                    <% unit "1"
+                      <h1><a href=NewPaste class="pjax">Happaste</a></h1>
+                    %>
+                  </div>
+                </div>
+              </div>
+              <div id="content">
+                <div class="grid">
+                  <div class="yui3-g">
+                    <% body %>
+                  </div>
+                </div>
+              </div>
+              <script src=(Asset "yui.js")/>
+              <% [$jmacro|
+                YUI().use "pjax" \y ->
+                  new y.Pjax { container: "#content"
+                             , linkSelector: "a.pjax"
+                             }
+              |] %>
+            </body>
+          </html>
   where
     stylesheet :: (XMLGenerator x, EmbedAsAttr x (Attr String url))
                => url -> XMLGenT x (HSX.XML x)
@@ -76,6 +85,6 @@ recentPastesList :: XMLGenT Server (HSX.XML Server)
 recentPastesList = query RecentPastes >>= \ps ->
     <ol class="recent-pastes">
       <% each ps $ \(k,p) ->
-        <li><a href=(ShowPaste k)><% p ^. fileName %></a></li>
+        <li><a href=(ShowPaste k) class="pjax"><% p ^. fileName %></a></li>
       %>
     </ol>
